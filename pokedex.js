ec240345fe3a6f2ac81ffb987a17b6d0122fb219
João Manoel <joaomanoel.cc@gmail.com>
2023-04-18 18:23:46 -0300

    nova versão
    agora buscando um único pokemon e listando várias características

--- pokedex.js
+++ pokedex.js
@@ -1,40 +1,79 @@
-var quantidade = document.getElementById('quantidade');
+var pokemonNumber = document.getElementById('pokemonNumber');
 
-document.querySelector('input[id=pesquisar]').addEventListener('click',()=>{
+document.querySelector('input[id=search]').addEventListener('click',()=>{
     
-    pegaPokemons(quantidade.value);
+    searchPokemon(pokemonNumber.value);
 
-    function capitalizar(string){
+    function capitalize(string){
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
     
-    function pegaPokemons(quantidade){
-        fetch('https://pokeapi.co/api/v2/pokemon?limit='+quantidade)
+    function searchPokemon(pokemonNumber){
+        var pokemonSprite = document.querySelector('.pokemonSprite');
+        var info1 = document.getElementById('info1');
+        var info2 = document.getElementById('info2');
+        var info3 = document.getElementById('info3');
+        var info4 = document.getElementById('info4');
+        var info5 = document.getElementById('info5');
+        
+        var abilities = [];
+        var types = [];
+        var stats = [];
+
+        fetch('https://pokeapi.co/api/v2/pokemon/'+pokemonNumber)
             .then(response => response.json())
-            .then(allpokemon => {
+            .then(pokemon => {
+                info1.innerHTML = '<h1>Abilities:</h1>';
+                info2.innerHTML = '<h1>Types:</h1>';
+                info3.innerHTML = '<h1>Stats:</h1>';
+                info4.innerHTML = '<h1>Height:</h1>';
+                info5.innerHTML = '<h1>Weight:</h1>';
+                
+                
+                pokemonSprite.innerHTML = '<img src="'+pokemon.sprites.other["official-artwork"].front_default+'"</img><br><h1>'+capitalize(pokemon.name)+'</h1>';
 
-                var pokemons = [];
-
-                allpokemon.results.map((valor) => {
-                    fetch(valor.url)
-                        .then(response => response.json())
-                        .then(singlePokemon => {
-                            pokemons.push({ nome: valor.name, imagem: singlePokemon.sprites.other["official-artwork"].front_default });
-
-                            if (pokemons.length == quantidade) {
-                                var pokemonBoxes = document.querySelector('.pokemonBoxes');
-                                pokemonBoxes.innerHTML = "";
-                                pokemons.map((valor) => {
-                                    pokemonBoxes.innerHTML += '<div class="pokemonBox"> <img src="' + valor.imagem + '"><p>' + capitalizar(valor.nome) + '</p></div>';
-                                })
-                            }
-                        })
+                pokemon.abilities.map((index)=>{
+                    abilities.push(index.ability.name);
                 })
+                pokemon.types.map((index)=>{
+                    types.push(index.type.name);
+                })
+                pokemon.stats.map((index)=>{
+                    stats.push(index.stat.name+': '+index.base_stat);
+                })
+                
+                info1.innerHTML += '<p>'+abilities.join(', ')+'</p>';
+                info2.innerHTML += '<p>'+types.join(', ')+'</p>';
+                info3.innerHTML += '<p>'+stats.join(', ')+'</p>';
+                info4.innerHTML += '<p>'+pokemon.height/10+' m</p>';
+                info5.innerHTML += '<p>'+pokemon.weight/10+' kg</p>';
+                
+
+                
 
             })
 
     }
 })
+
+// fetch('https://pokeapi.co/api/v2/pokemon/'+pokemonNumber)
+//             .then(response => response.json())
+//             .then(pokemon => {
+//                 info1.innerHTML = '<h1>Abilities:</h1>';
+//                 pokemonSprite.innerHTML = '<img src="'+pokemon.sprites.other["official-artwork"].front_default+'"</img><br><h1>'+capitalize(pokemon.name)+'</h1>'
+//                 pokemon.abilities.map((index)=>{
+//                     abilities.push(index.ability.name);
+                    
+//                 })
+//                 console.log(abilities[0]);
+//                 info1.innerHTML += '<p>'+abilities.join(', ')+'</p>';
+
+                
+
+//             })
+
+//     }
+// })
 
 
 
